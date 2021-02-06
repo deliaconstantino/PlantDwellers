e8fe89e5b62d008a072d919f3b4fbd442fc43f06
deliaconstantino <dconstan92@gmail.com>
2021-02-06 18:56:34 -0500

    added new plant view and individual home view

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,42 @@
 class PlantsController < ApplicationController
+
+  get '/plants' do
+    @plants = Plant.all
+    erb :'plants/index'
+  end
+
+  get '/plants/new' do
+    if !Helpers.is_logged_in?(session)
+      #add flash message here: must be logged in to see this info
+      redirect '/login'
+    else
+      @dweller = Helpers.current_user(session)
+    # binding.pry
+      erb :'plants/new'
+    end
+  end
+
+  post '/plants' do
+    # binding.pry
+    if !Helpers.is_logged_in?(session)
+      #add flash message here: must be logged in to see this info
+      redirect '/login'
+    else
+      plant = Plant.new(common_name: params[:common_name], scientific_name: params[:scientific_name], category: params[:category], size: params[:size], watering_schedule: params[:watering_schedule], rotation_schedule: params[:rotation_schedule], fertilization_schedule: params[:fertilization_schedule])
+      dweller = Helpers.current_user(session)
+      if params[:add_to_home] == "yes"
+        plant.location = params[:location]
+        dweller.plants << plant
+      end
+      redirect '/plants'
+    end
+  end
+
+  get '/plants/:id' do
+    @plant = Plant.find(params[:id])
+    @homes = Home.all.select do |home|
+      home.plants.include?(@plant)
+    end
+    erb :'plants/show'
+  end
 end
